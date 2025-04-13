bf3a160309a43c58c09bd2a94c7bedf0f8069fdb
bala <vbalakumar.cse@gmail.com>
2025-04-13 10:51:32 -0500

    removed edit in manage list

--- src/pages2/Profile.js
+++ src/pages2/Profile.js
@@ -161,7 +161,7 @@ const Profile = () => {
 
                           <div className="d-flex border rounded align-items-center gap-2 p-1 form-group">
                             {friend.balance === 0 && (
-                              <a className="text-danger border-end px-1 " onClick={() => removeFriend(friend.id)} style={{ cursor: "pointer" }} aria-label="Remove Friend">
+                              <a className="text-danger border-end1 px-1 " onClick={() => removeFriend(friend.id)} style={{ cursor: "pointer" }} aria-label="Remove Friend">
                                 <small>
                                   <i className="fas fa-trash-alt px-1"></i>
                                 </small>
@@ -170,7 +170,7 @@ const Profile = () => {
                             <a className="px-1 border-end text-decoration-none   text-success d-none" href={`/app2/friend?id=${friend.id}&friendemail=${friend.friendemail}`} aria-label="View Friend">
                               {/* <FaArrowRight className="me-1 text-success" /> */} View
                             </a>
-                            <a className="px-1 text-decoration-none   text-success" onClick={() => handleEditClick(friend)} style={{ cursor: "pointer" }} aria-label="Edit Friend">
+                            <a className="px-1 text-decoration-none   text-success d-none" onClick={() => handleEditClick(friend)} style={{ cursor: "pointer" }} aria-label="Edit Friend">
                               {/* <FaRegEdit className="me-1 text-success" /> */} Edit
                             </a>
                           </div>
