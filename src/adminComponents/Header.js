ab081ce16d9ab78a9ec167a41cf0b4429ede072b
bala <vbalakumar.cse@gmail.com>
2025-04-14 09:18:14 -0500

    minor fix

--- src/adminComponents/Header.js
+++ src/adminComponents/Header.js
@@ -3,17 +3,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../images/splitequal.png";
 const Header = ({ title }) => (
-  <header className="header bg-myapp-recipe-ai ">
-    <div className="d-flex justify-content-between align-items-center">
-      <div className="flex-grow-1">
-        <h1 className="titlename-recipe-ai mb-0 text-nowrap w-75 px-3 p-3 ">{title}</h1>{" "}
-      </div>
-      <div> {process.env.REACT_APP_ENV === "QA" && <small className="px-2">NP</small>}</div>
-
-      <div className="px-2">
-        <img src={logo} alt="Logo" className="rounded" width="45" />
-      </div>
-    </div>
+  <header className="header bg-myapp">
+    <h1 className="titlename bg2-myapp mb-0 px-3 p-3  w-75">{title}</h1>
+    {process.env.REACT_APP_ENV === "QA" && <small className="px-1">NonProd</small>}
+    <Link to="/" className="home-link px-3">
+      <a className="navbar-brand text-white" href="/">
+        <img src={logo} alt="Logo" className="" width="30" />
+      </a>
+    </Link>
   </header>
 );
 
--- src/components2/Header.js
+++ src/components2/Header.js
@@ -2,15 +2,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import home from "../images/home.png";
+import logo from "../images/splitequal.png";
 const Header = ({ title }) => (
   <header className="header bg-myapp">
     <h1 className="titlename bg2-myapp mb-0 px-3 p-3  w-75">{title}</h1>
     {process.env.REACT_APP_ENV === "QA" && <small className="px-1">NonProd</small>}
-    <Link to="/" className="home-link px-3 d-none">
-      <a className="navbar-brand text-white" href="/">
-        <img src={home} alt="Logo" className="" width="30" />
+    <div className="home-link px-3">
+      <a className="navbar-brand text-white" href="#">
+        <img src={logo} alt="Logo" className="" width="30" />
       </a>
-    </Link>
+    </div>
   </header>
 );
 
