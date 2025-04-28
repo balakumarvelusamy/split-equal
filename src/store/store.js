a78f5a1148d40cb2fcdce0744db9bec80a329305
bala <vbalakumar.cse@gmail.com>
2025-04-28 15:51:39 -0500

    redux encrypt

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage"; // default: localStorage
 import { combineReducers } from "redux";
-
+import { encryptTransform } from "redux-persist-transform-encrypt";
 import groupReducer from "./groupSlice"; // we will create this next
 
 const persistConfig = {
   key: "root",
   storage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_KEY,
+      onError: function (error) {
+        console.error("Encryption error", error);
+      },
+    }),
+  ],
 };
 
 const rootReducer = combineReducers({
