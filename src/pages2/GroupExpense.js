8e75e4dc1e0636bfed884a1597770406508945d9
bala <vbalakumar.cse@gmail.com>
2025-05-11 12:38:10 -0500

    fix

--- src/pages2/GroupExpense.js
+++ src/pages2/GroupExpense.js
@@ -301,7 +301,7 @@ const GroupExpense = () => {
           <div className="input-group w-auto">
             <input type="text" className="form-control w-75" placeholder="Search Groups" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
             {searchText && (
-              <Button className="form-control text-muted w-auto" size="sm" onClick={() => setSearchText("")}>
+              <Button className="form-control text-light w-auto" size="sm" onClick={() => setSearchText("")}>
                 x
               </Button>
             )}
